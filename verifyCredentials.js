4434d382eb05484b1d145fbe4b1212a7354083bd
Renat Zubairov <renat.zubairov@gmail.com>
2017-09-12 23:03:59 +0200

    Added action

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -4,5 +4,5 @@ const r2 = require('r2');
 // This function will be called by the platform to verify credentials
 module.exports = function verifyCredentials(credentials) {
     console.log('Credentials passed for verification %j', credentials);
-    return r2(`https://${credentials.baseURL}/api/v1/users?token=${credentials.token}&page=1`);
+    return r2(`https://${credentials.baseURL}/api/v1/users?token=${credentials.token}&page=1`).json;
 };
